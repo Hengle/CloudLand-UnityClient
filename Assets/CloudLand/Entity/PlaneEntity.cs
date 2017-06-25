65978951ca3bb6cd295d38241a4a7dfca8f69b1a
DefinitlyEvil <theevildays@gmail.com>
2017-06-25 14:11:16 +0800

    plane entity

---
+++ Assets/CloudLand/Entity/PlaneEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Google.Protobuf.Collections;
+using Org.Dragonet.Cloudland.Net.Protocol;
+using UnityEngine;
+
+public class PlaneEntity : Entity {
+    protected override void ApplyMeta(MapField<uint, SerializedMetadata.Types.MetadataEntry> meta)
+    {
+    }
+
+    protected override int GetSlotCount()
+    {
+        return 0;
+    }
+
+    protected override Vector3 GetSlotRelativePosition(int slot)
+    {
+        return Vector3.zero;
+    }
+}
